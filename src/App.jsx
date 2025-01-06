0d13644c730d0e4a584f31b2aa01d04a8cfac235
Prasad Pathare <prasadsachinist10@gmail.com>
2025-01-06 14:32:56 +0000

    Added Like & Dislike Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,46 @@ import Form from "./Form";
 
 function App() {
   const [books, setBooks] = useState([
-    "The Great Gatsby",
-    "The Catcher in the Rye",
-    "To Kill a Mockingbird",
-    "1984",
-    "Pride and Prejudice",
+    {
+      id: crypto.randomUUID(),
+      text: "The Great Gatsby",
+      likes: 0,
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 1984,
+      likes: 0,
+    },
   ]);
 
-  const updateBooks = (book) => {
-    setBooks([book, ...books]);
+  const addBook = (book) => {
+    const newBook = {
+      id: crypto.randomUUID(),
+      text: book,
+      likes: 0,
+    };
+    setBooks([newBook, ...books]);
   };
 
-  const deleteBook = (book) => {
-    
-  }
+  const deleteBook = (id) => {
+    setBooks(books.filter((b) => b.id !== id));
+  };
+
+  const likeBook = (id) => {
+    setBooks((prevState) => {
+      return prevState.map((book) =>
+        book.id === id ? { ...book, likes: book.likes + 1 } : book
+      );
+    });
+  };
+
+  const dislikeBook = (id) => {
+    setBooks((prevState) => {
+      return prevState.map((book) =>
+        book.id === id ? { ...book, likes: book.likes - 1 } : book
+      );
+    });
+  };
 
   return (
     <>
@@ -24,10 +50,19 @@ function App() {
         <h1>Booker</h1>
       </header>
 
-      <Form onSubmit={updateBooks} />
+      <Form onSubmit={addBook} />
 
       {books.map((book) => (
-        <pre key={book}>{book}</pre>
+        <pre key={book.id}>
+          <h3>{book.text}</h3>
+          <p>{book.likes}</p>
+
+          <div style={{ display: "flex", gap: "5px" }}>
+            <button onClick={() => likeBook(book.id)}>Like</button>
+            <button onClick={() => dislikeBook(book.id)}>Dislike</button>
+            <button onClick={() => deleteBook(book.id)}>Delete</button>
+          </div>
+        </pre>
       ))}
     </>
   );
