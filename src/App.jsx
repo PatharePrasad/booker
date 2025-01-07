4013c0bcdef10c30ddb9cca67d4a48d24924451d
Prasad Pathare <prasadsachinist10@gmail.com>
2025-01-07 14:03:28 +0000

    port fully to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,20 @@
 import { useReducer } from "react";
 import Form from "./Form";
-
-const booksReducer = () => {}
+import { booksReducer } from "./lib/reducer";
 
 function App() {
   const [books, dispatch] = useReducer(booksReducer, [
     {
       id: crypto.randomUUID(),
       text: "The Great Gatsby",
-      likes: 0,
+      likes: 4,
     },
     {
       id: crypto.randomUUID(),
-      text: 1984,
-      likes: 0,
+      text: "1984",
+      likes: 10,
     },
   ]);
-
-  const addBook = (book) => {
-    const newBook = {
-      id: crypto.randomUUID(),
-      text: book,
-      likes: 0,
-    };
-    setBooks([newBook, ...books]);
-  };
-
-  const deleteBook = (id) => {
-    setBooks(books.filter((b) => b.id !== id));
-  };
-
-  const likeBook = (id) => {
-    setBooks((prevState) => {
-      return prevState.map((book) =>
-        book.id === id ? { ...book, likes: book.likes + 1 } : book
-      );
-    });
-  };
-
-  const dislikeBook = (id) => {
-    setBooks((prevState) => {
-      return prevState.map((book) =>
-        book.id === id ? { ...book, likes: book.likes - 1 } : book
-      );
-    });
-  };
-
-  const handleSort = () => {
-    setBooks([...books].sort((a, b) => b.likes - a.likes));
-  };
 
   return (
     <>
@@ -56,8 +22,11 @@ function App() {
         <h1>Booker</h1>
       </header>
 
-      <Form onSubmit={(name) => dispatch({type: "ADD", payload: name})}
-       onSort={() => dispatch ({type: "SORT"})} />
+      <Form
+        onSubmit={(name) => dispatch({ type: "ADD", payload: name })}
+        onSort={() => dispatch({ type: "SORT" })}
+        onOrder={() => dispatch({ type: "ORDER" })}
+      />
 
       {books.map((book) => (
         <pre key={book.id}>
@@ -65,9 +34,21 @@ function App() {
           <p>{book.likes}</p>
 
           <div style={{ display: "flex", gap: "5px" }}>
-            <button onClick={() => dispatch({type: "LIKE", payload: book.id})}>Like</button>
-            <button onClick={() => dislikeBook(book.id)}>Dislike</button>
-            <button onClick={() => deleteBook(book.id)}>Delete</button>
+            <button
+              onClick={() => dispatch({ type: "LIKE", payload: book.id })}
+            >
+              Like
+            </button>
+            <button
+              onClick={() => dispatch({ type: "DISLIKE", payload: book.id })}
+            >
+              Dislike
+            </button>
+            <button
+              onClick={() => dispatch({ type: "DELETE", payload: book.id })}
+            >
+              Delete
+            </button>
           </div>
         </pre>
       ))}
